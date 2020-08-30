689ffb20c050e0d7b19b9fbd80bf9a44ab94780c
Sombuddha Chakravarty <sombuddha2016@gmail.com>
2020-08-30 16:10:07 +0530

    Updated portfolio template test

--- js/main.js
+++ js/main.js
@@ -1,65 +1,38 @@
-jQuery(document).ready(function($) {
+$(document).ready(function() {
 
-
-    /*======= Skillset *=======*/
-    
-    $('.level-bar-inner').css('width', '0');
-    
-    $(window).on('load', function() {
-
-        $('.level-bar-inner').each(function() {
-        
-            var itemWidth = $(this).data('level');
-            
-            $(this).animate({
-                width: itemWidth
-            }, 800);
-            
-        });
-
+    $(window).scroll(function() {
+      if($(this).scrollTop() > 550) { 
+          $('.navbar').addClass('solid');
+          $('.back-to-top').addClass('visible'); 
+      } else {
+          $('.navbar').removeClass('solid');
+          $('.back-to-top').removeClass('visible');  
+      }
     });
-    
-    /* Bootstrap Tooltip for Skillset */
-    $('.level-label').tooltip();
-    
-    
-    /* jQuery RSS - https://github.com/sdepold/jquery-rss */
-    
-    $("#rss-feeds").rss(
-    
-        //Change this to your own rss feeds
-        "http://feeds.feedburner.com/TechCrunch/startups",
-        
-        {
-        // how many entries do you want?
-        // default: 4
-        // valid values: any integer
-        limit: 3,
-        
-        // the effect, which is used to let the entries appear
-        // default: 'show'
-        // valid values: 'show', 'slide', 'slideFast', 'slideSynced', 'slideFastSynced'
-        effect: 'slideFastSynced',
-        
-        // outer template for the html transformation
-        // default: "<ul>{entries}</ul>"
-        // valid values: any string
-        layoutTemplate: "<div class='item'>{entries}</div>",
-        
-        // inner template for each entry
-        // default: '<li><a href="{url}">[{author}@{date}] {title}</a><br/>{shortBodyPlain}</li>'
-        // valid values: any string
-        entryTemplate: '<h3 class="title"><a href="{url}" target="_blank">{title}</a></h3><div><p>{shortBodyPlain}</p><a class="more-link" href="{url}" target="_blank"><i class="fa fa-external-link"></i>Read more</a></div>'
-        
-        }
-    );
-    
-    /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    GitHubCalendar("#github-graph", "IonicaBizau");
-    
-    
-    /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "caseyscarborough", selector: "#ghfeed" });
 
+    $("#scrollIcon").click(function(e) {
+        e.preventDefault();
+        $.scrollTo($("#about"), 1000);
+    });
 
-});
+    $("#nav-about").click(function(e) {
+        e.preventDefault();
+        $.scrollTo($("#about"), 1000);
+    });
+
+    $("#nav-portfolio").click(function(e) {
+        e.preventDefault();
+        $.scrollTo($("#portfolio"), 1000);
+    });
+
+    $("#nav-contact").click(function(e) {
+        e.preventDefault();
+        $.scrollTo($("#contact"), 1000);
+    });
+
+    $(".navbar-brand").click(function(e) {
+        e.preventDefault();
+        $.scrollTo(0, 1000);
+    });
+          
+  });
